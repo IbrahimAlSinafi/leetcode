b3a21fad73fb253f974aeaf43aea8e3127c04cf7
IbrahimAlSinafi <ifalsinafi@ucdavis.edu>
2018-07-07 16:27:40 -0700

    feat(reduce.js): add basic example to illustrate reduce

---
+++ reduce.js
@@ -0,0 +1,7 @@
+// https://medium.freecodecamp.org/reduce-f47a7da511a9
+// A Basic Reduction
+// Use it when: You have an array of amounts and you want to add them all up.
+const euros = [29.76, 41.85, 46.5];
+const sum = euros.reduce((total, amount) => total + amount); 
+sum // 118.11
+
