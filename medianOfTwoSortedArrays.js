6aa028aeeca93836c6f4fa0288b8b37a437033d0
IbrahimAlSinafi <ifalsinafi@ucdavis.edu>
2018-07-16 18:11:07 -0700

    feat(medianOfTwoSortedArray): solve another problem from leetcode

---
+++ medianOfTwoSortedArrays.js
@@ -0,0 +1,35 @@
+/*
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  nums1.forEach(ele => {
+    nums2.push(ele)
+  })
+    nums2.sort((a,b) => a - b)
+  if(nums2.length ===0) return 0
+  var half = Math.floor(nums2.length / 2);
+
+  if (nums2.length % 2)
+    return nums2[half];
+  else
+    return (nums2[half - 1] + nums2[half]) / 2.0;
+};
