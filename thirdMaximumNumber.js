0353555f680fbfd39f7a74b85c9762677b4231bd
IbrahimAlSinafi <ifalsinafi@ucdavis.edu>
2020-07-12 09:22:27 -0700

    solve thirdMaxNumber

---
+++ thirdMaximumNumber.js
@@ -0,0 +1,39 @@
+/**
+Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+Example 2:
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+ */
+
+ /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    const sortedUniqueArr = nums
+      .sort((a,b) => b-a)
+      .reduce((acc, val) => {
+      return acc.includes(val)? acc: [...acc, val];
+    }, [])
+
+    if (sortedUniqueArr.length >= 3) return sortedUniqueArr[2]
+    return sortedUniqueArr[0]
+    
+};
